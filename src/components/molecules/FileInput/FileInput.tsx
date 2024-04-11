162a671d52122b2b9fbb38b23fc9e4f9d11cfce5
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-04-11 10:57:29 +0200

    Implement `FileInput` component

--- src/components/molecules/FileInput/FileInput.tsx
+++ src/components/molecules/FileInput/FileInput.tsx
@@ -1,22 +1,35 @@
 'use client'
 import styles from './FileInput.module.css'
 import type { FileInputProps } from '@/components/molecules/FileInput/FileInput.types'
-import { mergeEventHandlers, mergeRefs } from '@/utils'
+import { mergeRefs } from '@/utils'
 import {
   ArrowUpTrayIcon as UploadIcon,
   TrashIcon,
   ExclamationCircleIcon as ErrorIcon,
+  DocumentDuplicateIcon as FileIcon,
 } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import { AnimatePresence, m } from 'framer-motion'
 import type { Variants } from 'framer-motion'
-import { forwardRef, useRef, useState, type ChangeEventHandler } from 'react'
+import Image from 'next/image'
+import { forwardRef, useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 
+// TODO: Resolve animation bugs
 const errorVariants: Variants = {
   hidden: { opacity: 0, y: '-50%', height: 0 },
   visible: { opacity: 1, y: 0, height: 'auto' },
 }
+const iconButtonVariants = {
+  hidden: { opacity: 0, x: '100%' },
+  visible: { opacity: 1, x: 0 },
+}
+const itemVariants = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+}
+
+type FileWithPreview = File & { preview: string }
 
 export const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
   (props, forwardedRef) => {
@@ -28,85 +41,156 @@ export const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
       dragPlaceholderHeading,
       dragPlaceholderDescription,
       error,
-      multiple,
-      onChange,
       ...restProps
     } = props
 
-    const [files, setFiles] = useState<File[]>([])
-    const inputRef = useRef<HTMLInputElement | null>(null)
+    const [files, setFiles] = useState<FileWithPreview[]>([])
 
-    const handleSetFiles: ChangeEventHandler<HTMLInputElement> = (event) => {
-      const currentFiles = event.target.files
-      if (!currentFiles) return
-      setFiles((prevFiles) => [...prevFiles, ...currentFiles])
+    const handleOnDrop = useCallback(
+      (acceptedFiles: File[]) => {
+        setFiles((prevFiles) => [
+          ...prevFiles,
+          ...acceptedFiles.map((file) =>
+            Object.assign(file, { preview: URL.createObjectURL(file) }),
+          ),
+        ])
+      },
+      [setFiles],
+    )
+
+    const {
+      open: handleOpen,
+      isDragActive,
+      getInputProps,
+      getRootProps,
+      inputRef,
+    } = useDropzone({
+      onDrop: handleOnDrop,
+      noClick: files.length > 0,
+    })
+
+    const handleDelete = (fileName?: string) => {
+      if (fileName === undefined) {
+        setFiles([])
+        return
+      }
+
+      setFiles((prevFiles) =>
+        prevFiles.filter((file) => file.name !== fileName),
+      )
     }
 
-    const { open, isDragActive, getInputProps, getRootProps } = useDropzone()
-    const { onChange: dropzoneOnChange, ...restDropzoneProps } = getInputProps()
-
     return (
-      <m.label
-        layout
-        className={clsx(styles.container, className)}
-        data-testid="input"
-        {...getRootProps<any>()}
+      <div
+        {...getRootProps({
+          className,
+        })}
       >
-        <div className="flex items-center justify-between">
-          <span
-            className={clsx(styles.label, error && styles.labelError)}
-            data-testid="input-label"
-          >
-            {label}
-          </span>
-          <div className="flex space-x-3">
-            <button
-              type="button"
-              className="rounded p-1 hover:bg-neutral-200"
-              onClick={open}
-            >
-              <UploadIcon className="size-5" />
-            </button>
-            <button
-              type="button"
-              className="rounded p-1 hover:bg-neutral-200"
-              onClick={() => setFiles([])}
-            >
-              <TrashIcon className="size-5" />
-            </button>
-          </div>
-        </div>
-        <div className={styles.wrapper}>
-          {files.length ? (
-            <div>
-              {files.map((file, i) => {
-                const reader = new FileReader()
-                reader.readAsDataURL(file)
-                const fileSrc = reader.result
-                return (
-                  <img
-                    className="aspect-[4/3] max-w-24"
-                    key={i}
-                    alt="asdf"
-                    src={fileSrc as string}
-                  />
-                )
-              })}
-            </div>
-          ) : (
-            <div className={styles.placeholder}>
-              <UploadIcon className={styles.placeholderIcon} />
-              <p className={styles.placeholderHeading}>
-                {isDragActive ? dragPlaceholderHeading : placeholderHeading}
-              </p>
-              <p className={styles.placeholderDescription}>
-                {isDragActive
-                  ? dragPlaceholderDescription
-                  : placeholderDescription}
-              </p>
-            </div>
+        <m.div
+          layout="size"
+          className={clsx(
+            styles.container,
+            error && styles.containerError,
+            isDragActive && styles.containerDragActive,
           )}
-        </div>
+        >
+          <m.div
+            layout="position"
+            className={clsx(styles.header, error && styles.headerError)}
+          >
+            <span
+              className={clsx(styles.label)}
+              data-testid="input-label"
+            >
+              {label}
+            </span>
+            <div className={styles.headerButtonsWrapper}>
+              <button
+                type="button"
+                className={styles.headerButton}
+                onClick={handleOpen}
+              >
+                <UploadIcon />
+              </button>
+              <AnimatePresence>
+                {files.length > 0 && (
+                  <m.button
+                    type="button"
+                    className={styles.headerButton}
+                    onClick={() => handleDelete()}
+                    variants={iconButtonVariants}
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                  >
+                    <TrashIcon />
+                  </m.button>
+                )}
+              </AnimatePresence>
+            </div>
+          </m.div>
+          <m.div
+            layout="position"
+            className={clsx(
+              styles.wrapper,
+              files.length === 0 && styles.placeholderWrapper,
+            )}
+          >
+            {files.length ? (
+              <ul className={styles.previewWrapper}>
+                <AnimatePresence mode="popLayout">
+                  {files.map(
+                    (file) =>
+                      file && (
+                        <m.li
+                          layout="position"
+                          variants={itemVariants}
+                          initial="hidden"
+                          animate="visible"
+                          exit="hidden"
+                          key={file.name}
+                          className={styles.previewItem}
+                        >
+                          <Image
+                            className={styles.previewImage}
+                            alt={file.name}
+                            width={200}
+                            height={100}
+                            onLoad={() => URL.revokeObjectURL(file.preview)}
+                            src={file.preview}
+                          />
+                          <button
+                            className={styles.previewButton}
+                            onClick={() => handleDelete(file.name)}
+                          >
+                            <TrashIcon />
+                          </button>
+                        </m.li>
+                      ),
+                  )}
+                </AnimatePresence>
+              </ul>
+            ) : (
+              <div
+                className={clsx(
+                  styles.placeholder,
+                  error && styles.placeholderError,
+                  isDragActive && styles.placeholderDragActive,
+                )}
+              >
+                <FileIcon className={styles.placeholderIcon} />
+                <p className={styles.placeholderHeading}>
+                  {isDragActive ? dragPlaceholderHeading : placeholderHeading}
+                </p>
+                <p className={styles.placeholderDescription}>
+                  {isDragActive
+                    ? dragPlaceholderDescription
+                    : placeholderDescription}
+                </p>
+              </div>
+            )}
+          </m.div>
+        </m.div>
         <AnimatePresence>
           {error && (
             <m.p
@@ -122,20 +206,13 @@ export const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
           )}
         </AnimatePresence>
         <input
-          type="file"
-          hidden
-          onChange={mergeEventHandlers(
-            onChange,
-            handleSetFiles,
-            dropzoneOnChange,
-          )}
-          ref={mergeRefs(inputRef, forwardedRef)}
-          className={clsx(styles.input, error && styles.inputError)}
-          {...restProps}
-          {...restDropzoneProps}
+          {...getInputProps({
+            ref: mergeRefs(inputRef, forwardedRef),
+            ...restProps,
+          })}
         />
-      </m.label>
+      </div>
     )
   },
 )
-FileInput.displayName = 'Input'
+FileInput.displayName = 'FileInput'
